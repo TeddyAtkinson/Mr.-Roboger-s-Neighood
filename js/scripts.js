bbd5e1272cac64ab6fc69926645b1f38a6fce48c
teddyatkinson <tedddy.atkinson@gmail.com>
2022-10-28 16:20:46 -0700

    Output array under results while removing hidden class

--- js/scripts.js
+++ js/scripts.js
@@ -9,30 +9,34 @@ window.addEventListener("load", function() {
 
 function result(event) {
   event.preventDefault();
-
-  const userInput = document.querySelector("input").value;
-  console.log(userInput);
   document.getElementById("results").setAttribute("class", "hidden");
 
-  let userArray = [];
-  const patternThree = new RegExp("[3]", "g");
-  const patternTwo = new RegExp("[2]", "g");
-  const patternOne = new RegExp("[1]", "g");
+  let userArray = [1,2];
+  const userInput = document.querySelector("input").value;
+  console.log(userInput);
+  
+  // const patternThree = new RegExp("[3]", "g");
+  // const patternTwo = new RegExp("[2]", "g");
+  // const patternOne = new RegExp("[1]", "g");
 
-  for (let i = 1; i >= parseInt(userinput); i++) {
-    if (i.match(patternThree) === true) {
-      userArray.push("Won't you be my neighbor?");
-    }
-    else if (i.match(patternTwo) === true) {
-      userArray.push("Boop!");
-    }
-    else if (i.match(patternOne) === true) {
-      userArray.push("Beep");
-    }
-    else {
-      userArray.push(i);
-    }
-  }
+  // for (let i = 0; i < userInput; i++) {
+  //   if (i.match(patternThree) === true) {
+  //     userArray.push("Won't you be my neighbor?");
+  //   }
+  //   else if (i.match(patternTwo) === true) {
+  //     userArray.push("Boop!");
+  //   }
+  //   else if (i.match(patternOne) === true) {
+  //     userArray.push("Beep");
+  //   }
+  //   else {
+  //     userArray.push(i);
+  //   }
+  // }
   console.log(userArray);
+  
+  document.getElementById("info").innerText = userArray
+  document.getElementById("results").removeAttribute("class");
+  
 }
 
