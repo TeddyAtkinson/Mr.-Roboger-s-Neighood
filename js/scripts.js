1435692b342f76cacb860e0f110ed67ab4dfea22
teddyatkinson <tedddy.atkinson@gmail.com>
2022-10-28 15:36:47 -0700

    Working: Logic for UserArray Input and Replacement

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,32 @@
+window.addEventListener("load", function() {
+  console.log("load event")
+  //document.getElementById("results").setAttribute("class", "hidden");
+  const input = document.getElementById("input");
+  input.addEventListener("submit", result);
+});
+
 //------------------------------------------------------------------------------
+
+function result(event) {
+  event.preventDefault();
+  //console.log("survey submited")
+
+  const selected = document.querySelector("input[name ='q5']:checked").value;
+  document.getElementById("results").setAttribute("class", "hidden");
+
+  let userArray = [];
+  
+  for (i = 1; i >= userinput, i++)
+    if (number contains 3) {
+    }
+      useArray.push("Won't you be my neighbor?");
+    else if (number contains 2){
+    }
+      useArray.push("Boop!");
+    else if (number contains 1){
+    }
+      useArray.push("Beep");
+    else () {
+      return useArray.push(i);
+    }
+}
