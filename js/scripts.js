eb99ad9e7d71189bb6e8d7318432d47d03827a96
teddyatkinson <tedddy.atkinson@gmail.com>
2022-10-28 16:07:19 -0700

    Working: Logic for array output

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 window.addEventListener("load", function() {
   console.log("load event")
-  //document.getElementById("results").setAttribute("class", "hidden");
+  document.getElementById("results").setAttribute("class", "hidden");
   const input = document.getElementById("input");
   input.addEventListener("submit", result);
 });
@@ -9,24 +9,30 @@ window.addEventListener("load", function() {
 
 function result(event) {
   event.preventDefault();
-  //console.log("survey submited")
 
-  const selected = document.querySelector("input[name ='q5']:checked").value;
+  const userInput = document.querySelector("input").value;
+  console.log(userInput);
   document.getElementById("results").setAttribute("class", "hidden");
 
   let userArray = [];
-  
-  for (i = 1; i >= userinput, i++)
-    if (number contains 3) {
+  const patternThree = new RegExp("[3]", "g");
+  const patternTwo = new RegExp("[2]", "g");
+  const patternOne = new RegExp("[1]", "g");
+
+  for (let i = 1; i >= parseInt(userinput); i++) {
+    if (i.match(patternThree) === true) {
+      userArray.push("Won't you be my neighbor?");
     }
-      useArray.push("Won't you be my neighbor?");
-    else if (number contains 2){
+    else if (i.match(patternTwo) === true) {
+      userArray.push("Boop!");
     }
-      useArray.push("Boop!");
-    else if (number contains 1){
+    else if (i.match(patternOne) === true) {
+      userArray.push("Beep");
     }
-      useArray.push("Beep");
-    else () {
-      return useArray.push(i);
+    else {
+      userArray.push(i);
     }
+  }
+  console.log(userArray);
 }
+
