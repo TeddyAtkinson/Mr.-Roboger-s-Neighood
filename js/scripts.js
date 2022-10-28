bb1c37b1b9e17b23c72273a29f4b376f33d6a0c4
teddyatkinson <tedddy.atkinson@gmail.com>
2022-10-28 16:36:50 -0700

    Output changes depending on if number contains 1/2/3

--- js/scripts.js
+++ js/scripts.js
@@ -11,32 +11,34 @@ function result(event) {
   event.preventDefault();
   document.getElementById("results").setAttribute("class", "hidden");
 
-  let userArray = [1,2];
+  let userArray = [];
   const userInput = document.querySelector("input").value;
-  console.log(userInput);
+  //console.log(userInput);
   
-  // const patternThree = new RegExp("[3]", "g");
-  // const patternTwo = new RegExp("[2]", "g");
-  // const patternOne = new RegExp("[1]", "g");
+  const patternThree = new RegExp("[3]", "g");
+  const patternTwo = new RegExp("[2]", "g");
+  const patternOne = new RegExp("[1]", "g");
+  
 
-  // for (let i = 0; i < userInput; i++) {
-  //   if (i.match(patternThree) === true) {
-  //     userArray.push("Won't you be my neighbor?");
-  //   }
-  //   else if (i.match(patternTwo) === true) {
-  //     userArray.push("Boop!");
-  //   }
-  //   else if (i.match(patternOne) === true) {
-  //     userArray.push("Beep");
-  //   }
-  //   else {
-  //     userArray.push(i);
-  //   }
-  // }
-  console.log(userArray);
-  
+  for (let i = 1; i <= userInput; i++) {
+    let iString = `${i}`;
+    console.log(iString);
+    if (iString.match(patternThree)) {
+      userArray.push(" 'Won't you be my neighbor?'");
+    }
+    else if (iString.match(patternTwo)) {
+      userArray.push(" 'Boop!'");
+    }
+    else if (iString.match(patternOne)) {
+      userArray.push(" 'Beep'");
+    }
+    else {
+      userArray.push(i);
+    }
+  }
+  //console.log(userArray);
+
   document.getElementById("info").innerText = userArray
   document.getElementById("results").removeAttribute("class");
-  
+  document.getElementById("results").setAttribute("class", "pformat");
 }
-
